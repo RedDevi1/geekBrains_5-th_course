95ec5da89df6e7f78b8e3b4a9b097450b0887c42
Nikita <nickita.sevsk@gmail.com>
2022-04-04 16:22:16 +0300

    Lesson_1_begin

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        private readonly HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {             
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, $"/posts/{id}");
+            try
+            {
+                HttpResponseMessage response = client.SendAsync(httpRequest).Result;
+                using var responseStream = response.Content.ReadAsStreamAsync().Result;
+
+                var metricsResponse = JsonSerializer.DeserializeAsync<AllHddMetricsApiResponse>(responseStream).Result;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
