597c684ccec0cb675e02a2bb1fd88656bc38d5ca
Nikita <nickita.sevsk@gmail.com>
2022-04-13 16:13:40 +0300

    Lesson_3_begin

---
+++ SOLIDWebApplication/DAL/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOLIDWebApplication.DAL.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IList<T> GetAll();
+        T GetById(int Id);
+        void Create(T item);
+        void Update(int id);
+        void Delete(int id);
+    }
+}
---
+++ SOLIDWebApplication/DAL/Repositories/PersonsRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SOLIDWebApplication.DAL.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SOLIDWebApplication.Models;
+
+namespace SOLIDWebApplication.DAL.Repositories
+{
+    public class PersonsRepository : IPersonsRepository
+    {
+        void IRepository<Person>.Create(Person item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<Person>.Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IList<Person> IRepository<Person>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Person IRepository<Person>.GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<Person>.Update(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
