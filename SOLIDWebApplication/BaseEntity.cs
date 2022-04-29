15a4e410a6958a4dd70b5101e623d9bac0df2fa7
nickita-piter@rambler.ru <nickita-piter@rambler.ru>
2022-04-29 16:24:29 +0300

    Lesson_4_begin

---
+++ SOLIDWebApplication/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SOLIDWebApplication
+{
+    public class BaseEntity<TUniqueId> where TUniqueId : struct
+    {
+        public TUniqueId Id { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    [Table("User", Schema = "Test")]
+    public sealed class User : BaseEntity<int>
+    {
+        public string Comment { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+    }
+}
---
+++ SOLIDWebApplication/UserDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SOLIDWebApplication
+{
+    internal sealed class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder
+        optionsBuilder)
+        {
+            optionsBuilder
+            .UseNpgsql("Host = 192.168.1.72; Database = GeekBrains; Username = postgres; Password = qwe123; ");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().Ignore(x => x.Comment);
+        }
+    }
+}
