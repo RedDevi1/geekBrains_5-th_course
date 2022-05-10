18f18f8a3cb2faa88ef43248bde1df0b4676d5b7
Nikita Dernov <nickita-piter@rambler.ru>
2022-05-10 22:06:43 +0300

    Lesson_5_begin

--- SOLIDWebApplication/DAL/Interfaces/IPersonsService.cs
+++ SOLIDWebApplication/DAL/Interfaces/IPersonsService.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using SOLIDWebApplication.Models;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using SOLIDWebApplication.DAL.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityModel.Client;
 
 namespace SOLIDWebApplication.DAL.Interfaces
 {
     public interface IPersonsService : IService<PersonDTO>
     {
+        TokenResponse Authenticate(string user, string password);
     }
 }
--- SOLIDWebApplication/DAL/Services/PersonsService.cs
+++ SOLIDWebApplication/DAL/Services/PersonsService.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using SOLIDWebApplication.Models;
 using AutoMapper;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 
 namespace SOLIDWebApplication.DAL.Services
 {
@@ -12,16 +16,61 @@ namespace SOLIDWebApplication.DAL.Services
     {
         private IPersonsRepository repository;
         private readonly IMapper mapper;
+        public const string SecretCode = "THIS IS SOME VERY SECRET STRING!!! Im blue da ba dee da ba di da ba dee da ba di da d ba dee da ba di da badee";
+        private IDictionary<string, string> _users = new Dictionary<string, string>()
+        {
+            {"test", "test"}
+        };
         public PersonsService(IPersonsRepository repository, IMapper mapper)
         {
             this.repository = repository;
             this.mapper = mapper;
         }
+
+        public string Authenticate(string user, string password)
+        {
+            if (string.IsNullOrWhiteSpace(user) ||
+            string.IsNullOrWhiteSpace(password))
+            {
+                return string.Empty;
+            }
+            int i = 0;
+            foreach (KeyValuePair<string, string> pair in _users)
+            {
+                i++;
+                if (string.CompareOrdinal(pair.Key, user) == 0 &&
+                string.CompareOrdinal(pair.Value, password) == 0)
+                {
+                    return GenerateJwtToken(i);
+                }
+            }
+            return string.Empty;
+        }
+
+        private string GenerateJwtToken(int id)
+        {
+            JwtSecurityTokenHandler tokenHandler = new
+            JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(SecretCode);
+            SecurityTokenDescriptor tokenDescriptor = new
+            SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(15),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
         public IReadOnlyList<PersonDTO> GetAll()
         {
             IReadOnlyList<Person> persons = repository.GetAll();
             var personsDTOList = new List<PersonDTO>();
-            foreach(var person in persons)
+            foreach (var person in persons)
             {
                 personsDTOList.Add(mapper.Map<PersonDTO>(person));
             }
