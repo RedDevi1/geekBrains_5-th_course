a730dbe72458d7708db3e83bdc75516cbe0b6709
nickita-piter@rambler.ru <nickita-piter@rambler.ru>
2022-05-13 16:22:12 +0300

    some_updates

---
+++ Matrix/Program.cs
@@ -0,0 +1,33 @@
+﻿var matrixSize = 5;
+var matrix = new int[matrixSize, matrixSize];
+var cnt = 0;
+var indent = 0;
+while (cnt < 26)
+{
+    for (var i = 0; i < matrixSize; i++)
+    {
+        indent++;
+        if (i == 0 && cnt == 0)
+        {
+            for (var j = 0; j < matrixSize; j++)
+            {
+                cnt++;
+                matrix[i, j] = cnt;
+            }
+        }
+        else if (i > 0 && i < matrixSize - 1)
+        {
+            cnt++;
+            matrix[i, matrixSize - indent] = cnt;
+        }
+        else if (i == matrixSize - 1 && cnt < 15)
+        {
+            for (var j = matrixSize - 1; j >= 0; j--)
+            {
+                cnt++;
+                matrix[i, j] = cnt;
+            }
+        }
+    }
+}
+
